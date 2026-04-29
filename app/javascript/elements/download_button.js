661878a1ea5c706b4cdccc575a55fa6f86817a20
jbstanley2004 <jbstanley2004@gmail.com>
2026-04-29 10:38:07 -0400

    Preserve embedded signer download fallback

--- app/javascript/elements/download_button.js
+++ app/javascript/elements/download_button.js
@@ -19,71 +19,70 @@ export default targetable(class extends HTMLElement {
     this.toggleState()
 
     fetch(this.dataset.src, withSafeFetchOptions()).then(async (response) => {
-      if (response.ok) {
-        const urls = await response.json()
-        const isMobileSafariIos = 'ontouchstart' in window && navigator.maxTouchPoints > 0 && /AppleWebKit/i.test(navigator.userAgent)
-        const isSafariIos = isMobileSafariIos || /iPhone|iPad|iPod/i.test(navigator.userAgent)
+      if (!response.ok) throw new Error('Failed to resolve download URLs')
 
-        if (isSafariIos && urls.length > 1) {
-          this.downloadSafariIos(urls)
-        } else {
-          this.downloadUrls(urls)
-        }
-      } else {
-        alert('Failed to download files')
+      const urls = await response.json()
+      const isIos = /iPhone|iPad|iPod/i.test(navigator.userAgent)
+
+      if (urls.length === 1) {
+        this.openDirectUrl(urls[0])
+        return
       }
+
+      if (isIos) {
+        this.downloadSafariIos(urls)
+      } else {
+        this.downloadUrls(urls)
+      }
+    }).catch(() => {
+      alert('Failed to download files')
+    }).finally(() => {
+      this.toggleState()
     })
+  }
+
+  openDirectUrl (url) {
+    window.location.assign(url)
+  }
+
+  clickLink (link) {
+    document.body.appendChild(link)
+    link.click()
+    link.remove()
   }
 
   downloadUrls (urls) {
     const fileRequests = urls.map((url) => {
       return () => {
         return fetch(url).then(async (resp) => {
+          if (!resp.ok) throw new Error('Failed to download file')
+
           const blobUrl = URL.createObjectURL(await resp.blob())
           const link = document.createElement('a')
 
           link.href = blobUrl
           link.setAttribute('download', decodeURI(url.split('/').pop()))
 
-          link.click()
+          this.clickLink(link)
 
-          URL.revokeObjectURL(blobUrl)
+          setTimeout(() => URL.revokeObjectURL(blobUrl), 1000)
+        }).catch(() => {
+          this.openDirectUrl(url)
         })
       }
     })
 
-    fileRequests.reduce(
+    return fileRequests.reduce(
       (prevPromise, request) => prevPromise.then(() => request()),
       Promise.resolve()
-    ).finally(() => {
-      this.toggleState()
-    })
+    )
   }
 
   downloadSafariIos (urls) {
-    const fileRequests = urls.map((url) => {
-      return fetch(url).then(async (resp) => {
-        const blob = await resp.blob()
-        const blobUrl = URL.createObjectURL(blob.slice(0, blob.size, 'application/octet-stream'))
-        const link = document.createElement('a')
-
-        link.href = blobUrl
-        link.setAttribute('download', decodeURI(url.split('/').pop()))
-
-        return link
-      })
-    })
-
-    Promise.all(fileRequests).then((links) => {
-      links.forEach((link, index) => {
-        setTimeout(() => {
-          link.click()
-
-          URL.revokeObjectURL(link.href)
-        }, index * 50)
-      })
-    }).finally(() => {
-      this.toggleState()
+    urls.forEach((url, index) => {
+      setTimeout(() => {
+        this.openDirectUrl(url)
+      }, index * 250)
     })
   }
 })
